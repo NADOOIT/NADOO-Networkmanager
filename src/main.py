25400077078d69d7022a7e683e95294d9a243bd1
Hani Bashir <20566958+hanibashir@users.noreply.github.com>
2024-07-18 17:19:43 +0200

    creating Kurzpraesentation slide from a template

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.app import NetworkManagerApp
+
+
+def main():
+    return NetworkManagerApp('Network_Manager', 'de.nadooit.network_manager')
+
+
+if __name__ == '__main__':
+    main().main_loop()
