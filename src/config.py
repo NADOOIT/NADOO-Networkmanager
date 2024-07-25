7373ce0f5d34afd48fc9697074631a86c987dbaf
Hani Bashir <20566958+hanibashir@users.noreply.github.com>
2024-07-25 16:55:17 +0200

    add .env.example

--- src/config.py
+++ src/config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BENUTZER_DATEN_FILE = os.getenv('BENUTZER_DATEN_FILE', 'data/db/benutzer_data.json')
-FOLIEN_DATEN_FILE = os.getenv('FOLIEN_DATEN_FILE', 'data/db/folienvorlagen_data.json')
-KURZPRAESENTATION_FOLIEN_DATEN_FILE = os.getenv('KURZPRAESENTATION_FOLIEN_DATEN_FILE',
-                                                'data/db/benutzer_kurzpraesentation_data.json')
+BENUTZER_DATEN_FILE = os.getenv('BENUTZER_DATEN_FILE', '')
+FOLIEN_DATEN_FILE = os.getenv('FOLIEN_DATEN_FILE', '')
+KURZPRAESENTATION_FOLIEN_DATEN_FILE = os.getenv('KURZPRAESENTATION_FOLIEN_DATEN_FILE', '')
